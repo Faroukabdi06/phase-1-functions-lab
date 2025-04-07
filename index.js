220f20e68ff4bcbcf00f44b5a82731792bcf70b1
Farouk Abdi <abdifarouk58@gmail.com>
2025-04-07 17:08:08 +0300

    index.js updated

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    let Hq = 42;
+    return Math.abs(street - Hq);
+}
+distanceFromHqInBlocks(50);
+
+function distanceFromHqInFeet(street){
+    return distanceFromHqInBlocks(street) * 264;
+}
+distanceFromHqInFeet(50);
+
+function distanceTravelledInFeet(start, destination){
+    let value = Math.abs(destination - start);
+    return value * 264;
+}
+// console.log(distanceTravelledInFeet(50,58));
+
+function calculatesFarePrice(start,destination){
+    let feet = Math.abs(destination - start) * 264 ;
+    let  price = 0.02;
+    if(feet <= 400){
+        return 0;
+    }
+    else if(feet > 400 && feet<= 2000){
+        return price * (feet - 400)
+          }
+    else if(feet >= 2000 && feet <= 2500){
+      return 25
+    }
+    else{
+        return "cannot travel that far"
+    }
+
+}
+console.log(calculatesFarePrice(50, 58 ))
